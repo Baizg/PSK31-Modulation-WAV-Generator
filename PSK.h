1739f09004f3487beaac2e459fcc24b408050c28
Joshua Jerred <jdjerred@gmail.com>
2022-11-23 12:33:55 -0700

    Initial

---
+++ PSK.h
@@ -0,0 +1,20 @@
+/**
+ * @file PSK.h
+ * @author Joshua Jerred (https://joshuajer.red)
+ * @brief Header file for that defines the PSK class, varicode to ascii
+ * map, ascii to varicode loopup table, and convolutional code map.
+ * @date 2022-11-23
+ * @copyright Copyright (c) 2022
+ * @version 1.0
+ */
+
+#ifndef PSK_H_
+#define PSK_H_
+
+class PSK {
+    public:
+
+    private:
+};
+
+#endif
